a924c4e1fa699bd1286a43aea6aec9ca242f911a
RobinBilobrk <bobo.gamer66@gmail.com>
2019-02-07 10:31:07 +0100

    initial commit

---
+++ lobby.js
@@ -0,0 +1,16 @@
+
+var buttonOne = document.getElementsByName("but1")[0];
+
+    buttonOne.addEventListener("click", join1)
+
+
+
+
+
+function join1(){
+
+
+    console.log("Joined room 1");
+
+
+}
---
+++ lobby.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="lobby.css">
+    <script defer src="lobby.js"></script>
+    <title>Color warriors</title>
+  </head>
+  <body>
+  
+  <main>
+  <h1> Game section</h1>
+</main>    
+
+    
+
+  <aside>
+    <h1>Lobby</h1>
+
+    <div>Room 1</div>
+    <button type="button" name="but1">Join</button>
+    <div>Room 2</div>
+    <button type="button" name="but2">Join</button>
+    <div>Room 3</div>
+    <button type="button" name="but3">Join</button>
+
+  </aside>
+
+
+  </body>
+</html>
